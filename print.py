3ad74d3569c0d05a8520f32ce23b6915f807212b
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-23 20:04:43 +0300

    one day print

---
+++ print.py
@@ -0,0 +1,23 @@
+# print('Hello, World!')
+
+# print('I', 'really', 'love', 'Python')
+
+
+# print(2 + 3)
+# print(5 - 2)
+# print(4 * 7)
+# print(9 / 3)
+# print(2 ** 3)
+# print(8 + 4 / 2)
+# print((8 + 4) / 2)
+
+
+# print(42, 'is a great', 'number', sep='-')
+
+
+# print('Hello,', 'World!', end='!!!')
+
+
+# print(4, 3, sep='*', end=' ')
+
+# print('Done:', 4 * 3, sep=' -> ')
