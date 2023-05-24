28458d335185cc33bfb1cad811a35670a97bb1ee
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-24 16:39:43 +0300

    Two day , input function

---
+++ twoday/input.py
@@ -0,0 +1,21 @@
+# Example One
+organization = input()
+url = input()
+
+print('Analyzing the url', url, 'for organization', organization)
+
+# Example Two
+print('Enter your organization name', end=': ')
+organization = input()
+print('Enter the website URL', end=': ')
+url = input()
+
+print('Analyzing the url', url, 'for organization', organization)
+
+# Example Three
+print('Enter your organization name', end=': ')
+organization = input()
+print('Enter the website URL', end=': ')
+url = input()
+
+print('Analyzing the url', url, 'for organization', organization)
