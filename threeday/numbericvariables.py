cc7fedeac29665af5fc4c82237635c0b16d6d687
ayhan dasyuvarlar <ayhandasyuvarlar3@gmail.com>
2023-05-25 15:35:07 +0300

    numeric variables

---
+++ threeday/numbericvariables.py
@@ -0,0 +1,38 @@
+# Basic arithmetic operators
+print(3 + 2)  # Output: 5
+print(3 - 2)  # Output: 1
+print(3 * 2)  # Output: 6
+print(3 / 2)  # Output: 1.5
+print(3 // 2)  # Output: 1
+print(3 % 2)  # Output: 1
+print(3 ** 2)  # Output: 9
+
+# Order of operations
+print(3 + 2 * 4 - 1)
+# 3 + 2 * 4 - 1
+#  = 3 + 8 - 1
+#  = 11 - 1
+#  = 10
+
+# Using parentheses to specify the order of operations
+print((3 + 2) * 4 - 1)
+# (3 + 2) * 4 - 1
+#  = 5 * 4 - 1
+#  = 20 - 1
+#  = 19
+
+
+# Assigning values to variables
+x = 3
+y = 2
+print(x + y)  # Output: 5
+print(x * y)  # Output: 6
+
+# Getting integer inputs
+num = input('Enter an integer: ')  # Get input from the user
+num = int(num)                     # Convert the input to an integer
+print(num)                         # Print the integer
+
+
+age = int(input('Enter an integer: '))  # Get input from the user
+print(2 * age)                          # Print twice the age
